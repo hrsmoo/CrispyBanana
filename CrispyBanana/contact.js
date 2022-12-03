f7ae82d6fb6cf92c8245ec3351718c07b29b4a73
Haris Iskandar <93584750+hrsmoo@users.noreply.github.com>
2022-12-03 17:16:22 +0700

    Add files via upload

---
+++ CrispyBanana/contact.js
@@ -0,0 +1,16 @@
+function chat() {
+   const message = "hai min.. saya mau tanya tentang produk nya"
+   const whatsapp = "https://wa.me/+6289674335077"
+   open(whatsapp, "_blank")
+}
+
+function instagram() {
+   window.location.href = "https://instagram.com/ris.hariis"
+}
+
+function email() {
+   window.location.href = "mailto:hariis.iskandar03@gmail.com"
+}
+function fb() {
+   window.location.href = "https://www.facebook.com/hariis.iskandar"
+}
